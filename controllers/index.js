191e0e2e4b039b7c45297f044e6905d9ec8c424e
Antonela Porta <porta.antonela@gmail.com>
2022-10-19 22:18:29 -0300

    Fix bug at routes/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,8 @@
-const express = require('express')
+const { response } = require('express')
 const { statusModels } = require("../models/index")
 
-const getRootController = (req, res = express.response) => {
-    res.status(200).send(statusModels)
+const getRootController = (req, res = response) => {
+    res.status(200).send(statusModels())
 }
 
 module.exports = {
